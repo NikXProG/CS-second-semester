c1f0081fb53a09f85219cc32c0d42779ef8d2085
NIKX <tiger.zubairov@gmail.com>
2024-06-05 03:16:00 +0300

    exem4

--- CSharp2sem/Program.cs
+++ CSharp2sem/Program.cs
@@ -10,98 +10,156 @@ namespace CSharp2sem
     {
         public class Cache
         {
+
             private readonly  TimeSpan _timeLifeRecord;
             private readonly Int32 _maxCountRecord;
-            private readonly Dictionary<string, DateTime> _timeLifeRecords = new Dictionary<string, DateTime>();
-            public readonly Dictionary<string, object> _cache = new Dictionary<string, object>();
-            
-            public Cache(TimeSpan timeLifeRecord, Int32 maxCountRecord) { 
-                this._timeLifeRecord = timeLifeRecord;
-                this._maxCountRecord = maxCountRecord;
-                _cache.EnsureCapacity(maxCountRecord);
-                _timeLifeRecords.EnsureCapacity(maxCountRecord);
+            private readonly Dictionary<string, DateTime> _timeLifeRecords;
+            private readonly Dictionary<string, object> _cache;
+            private readonly Timer _timer;
+
+            public Cache(TimeSpan timeLifeRecord, Int32 maxCountRecord) {
+                _timeLifeRecord = timeLifeRecord;
+                _maxCountRecord = maxCountRecord;
+                _timeLifeRecords = new Dictionary<string, DateTime>();
+                _cache = new Dictionary<string, object>();
+                _timer = new Timer(CheckExpiredRecords!, null, timeLifeRecord, timeLifeRecord);
             }
 
-            public object? Get(string key)
+            public object Get(string key)
             {
+                lock (_cache) { 
+                    if (!_cache.ContainsKey(key))
+                    {
+                        throw new KeyNotFoundException();
+                    }
 
-                if (!(_cache.ContainsKey(key)))
-                {
-                    throw new KeyNotFoundException();
+                    return _cache[key];
                 }
-
-                return _cache[key];
             }
-            public async void Save(object CacheData, string key)
+            public void Save(object cacheData, string key)
             {
-                
+
+                lock(_cache){
+
+                    if (_cache.ContainsKey(key))
+                    {
+                        throw new ArgumentException("Ключ уже существует в кэше.");
+                    }
+
+                    if (_cache.Count == _maxCountRecord)
+                    {
+                        RemoveOldestObject(); // удаление самого старого объекта
+                    }
+
+                    _cache.Add(key, cacheData);
+                    DateTime timeNow = DateTime.Now;
+                    _timeLifeRecords.Add(key, timeNow);
+
+                }    
  
-                if ( _cache.ContainsKey(key) ){
-                    throw new ArgumentException();
-                }
-
-                if (_cache.Count == _maxCountRecord)
-                {
-
-                    RemovedOldestObject(); // удаление самого старого объекта
-
-                }
-
-                _cache.Add(key, CacheData);
-                DateTime timeNow = DateTime.Now;
-                _timeLifeRecords.Add(key, timeNow);
-                await ClearCacheAsync();   // вызов асинхронного метода
-                Thread.Sleep(4000);
-
-                Console.WriteLine(_cache.Count);
+ 
             }
-            private void RemovedOldestObject()
+            private void RemoveOldestObject()
             {
-                
                 DateTime timeMax = DateTime.MaxValue;
                 string? key = null;
+
                 foreach (var item in _timeLifeRecords)
                 {
-
                     if (item.Value < timeMax)
                     {
-                        timeMax = item.Value;//DateTime.Now.TimeOfDay - timeMax.TimeOfDay;
+                        timeMax = item.Value;
                         key = item.Key;
                     }
-            
                 }
+
                 if (key != null)
                 {
                     _timeLifeRecords.Remove(key);
                     _cache.Remove(key);
                 }
+            }
 
-            }
-            private async Task ClearCacheAsync()
+            private void CheckExpiredRecords(object state)
             {
-                Console.WriteLine("Начало метода PrintAsync"); // выполняется синхронно
-                await Task.Run(() => Print());
-                Console.WriteLine("Конец метода PrintAsync");
-            }
-            void Print()
-            {
-                Thread.Sleep(3000);     // имитация продолжительной работы
-                Console.WriteLine("Hello METANIT.COM");
+                lock (_cache)
+                {
+                    List<string> expiredKeys = new List<string>();
+
+                    foreach (var kvp in _timeLifeRecords)
+                    {
+                        if (DateTime.Now - kvp.Value > _timeLifeRecord)
+                        {
+                            expiredKeys.Add(kvp.Key);
+                        }
+                    }
+
+                    foreach (var key in expiredKeys)
+                    {
+                        _timeLifeRecords.Remove(key);
+                        _cache.Remove(key);
+                    }
+                }
             }
 
         }
-        public static int Main()
+        public static async Task<int> Main()
         {
-            TimeSpan time = new TimeSpan(0, 0, 5, 0);
-            var cache = new Cache(time, 2);
-            cache.Save("sdsds", "1");
-            Console.WriteLine(cache.Get("1"));
-            cache.Save("sdsSDSDSds", "2");
-            Console.WriteLine(cache.Get("2"));
-            cache.Save("sdsSSDFFDSDSDSds", "0");
-            Console.WriteLine(cache.Get("0"));
 
-            return 0;
+            try
+            {
+                TimeSpan time = TimeSpan.FromSeconds(3);
+                var cache = new Cache(time, 2);
+
+                var task = Task.Run(() =>
+                {
+                    cache.Save("sdsds", "1");
+                    Console.WriteLine($"Выполнился первый поток. Данные записи: {cache.Get("1")}");
+                });
+
+
+                var task1 = Task.Run(() => 
+                {
+                    cache.Save("SDSADASDSADSAD", "2");
+                    Console.WriteLine($"Выполнился второй поток. Данные записи: {cache.Get("2")}");
+                });
+
+                await Task.WhenAll(task, task1);
+
+                await Task.Delay(4000); // Ждем время
+
+                var task2 = Task.Run(() =>
+                {
+                    cache.Save("adasdsadsadsaSDSAdsadasd", "3");
+                    Console.WriteLine($"Выполнился третий поток. Данные записи: {cache.Get("3")}");
+                });
+
+                var task3 = Task.Run(() =>
+                {
+                    cache.Save("sdssdsdsds", "4");
+                    Console.WriteLine($"Выполнился четвертый поток. Данные записи: {cache.Get("4")}");
+                });
+
+                await Task.WhenAll(task2, task3);
+
+                Console.WriteLine(cache.Get("1"));
+
+                return 0;
+
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -2;
+            }
+
+
+
         }
 
     }
